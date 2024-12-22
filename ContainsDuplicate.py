b51c974875e39d69d27e8a68443c0c0a1e4e5613
Mervin Nguyen <mervin1@uci.edu>
2024-12-21 22:14:06 -0800

    Contains Duplicate using Hashmap data structure

---
+++ ContainsDuplicate.py
@@ -0,0 +1,15 @@
+#Title: Contains Duplicate
+
+# Given an integer array nums, return true if any value appears more than once in the array, otherwise return false. 
+
+class Solution:
+    #function to track duplicates
+    #paramenter: nums: List[int] -> true or false
+    def hasDuplicate(self, nums: List[int]) -> bool: 
+        hashmap = {}
+        for i in nums:
+            if i in hashmap:
+                return True    #iterate through each integer in nums, if the integer is in hashmap, return 
+            #add integer to hashmap
+            hashmap[i] = 1
+        return False
