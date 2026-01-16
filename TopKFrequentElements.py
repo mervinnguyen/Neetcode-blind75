9d63ca4dfaa26eac9d584443a0b252f68f7fade3
Mervin Nguyen <mervin1@uci.edu>
2026-01-15 21:50:29 -0800

    Add test case

--- TopKFrequentElements.py
+++ TopKFrequentElements.py
@@ -6,29 +6,32 @@
 #Output: [2,3]
 
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
     #Count the frequency of each indice in the list and increment as the number appears
 
     #Hashmap to keep track of the frequency of each number
-    frequency = {}
+        frequency = {}
 
-    #iterate through each index of num and the frequency
-    for n in nums:
-        frequency[n] = frequency.get(n, 0) + 1
-    
-    #Now that frequency = {1:1, 2:2, 3:3}
+        #iterate through each index of num and the frequency
+        for n in nums:
+            frequency[n] = frequency.get(n, 0) + 1
+        
+        #Now that frequency = {1:1, 2:2, 3:3}
 
-    #Sort in array in this order (frequency, nums)
-    array = []
-    for nums, freq in frequency.items():
-        array.append((freq, nums))
-    array.sort()
+        #Sort in array in this order (frequency, nums)
+        array = []
+        for nums, freq in frequency.items():
+            array.append((freq, nums))
+        array.sort()
 
-    inc = 0
-    result = []
-    while inc < k:
-        result.append(array.pop()[1])
-        inc += 1
-    
-    return result
+        inc = 0
+        result = []
+        while inc < k:
+            result.append(array.pop()[1])
+            inc += 1
+        
+        return result
 
+#test case
+s = Solution()
+print(s.topKFrequent([1,1,1,2,2,3], 2))
