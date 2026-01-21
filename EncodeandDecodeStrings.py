90da586da635634b02c35f05d6a6af1b07b8f3bc
Mervin Nguyen <mervin1@uci.edu>
2026-01-21 02:16:59 -0800

    Add Encode and Decode Strings to folder

---
+++ EncodeandDecodeStrings.py
@@ -0,0 +1,10 @@
+#Design an algorithm to encode a list of strings to a string. The
+#encoded string is then sent over the network and is decoded back to the original list of strings
+
+
+
+class Solution: 
+    def encode(self, strs: List[str]) -> str:
+    
+    def decode(self, strs: List[str]) -> str:
+        
