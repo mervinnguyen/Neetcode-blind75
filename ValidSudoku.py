44895b587351a58a63a0439b40fdf7c3caa4162a
Mervin Nguyen <mervin1@uci.edu>
2026-01-28 04:55:05 -0800

    Solution to ValidSudoku.py

--- ValidSudoku.py
+++ ValidSudoku.py
@@ -5,7 +5,7 @@
 #Return true if the sudoku board ius valid, otherwise return false. 
 
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
         #Check each row and make sure it contains 1-9 without duplicates
         for row in range(9):
             seen = set()
@@ -14,7 +14,7 @@ class Solution:
                     continue
                 if board[row][i] in seen:
                     return False        # return false if there are duplicates from 1-9
-            seen.add(board[row][i])
+                seen.add(board[row][i])
         
         #Check each column
         for column in range(9):
@@ -24,6 +24,43 @@ class Solution:
                     continue
                 if board[i][column] in seen:
                     return False
-            seen.add(board[i][column])
+                seen.add(board[i][column])
+        
+        for square in range(9):
+            seen = set()
+            for i in range(3):
+                for j in range(3):
+                    row = (square // 3) * 3 + i
+                    col = (square % 3) * 3 + j
+                    if board[row][col] == ".":
+                        continue
+                    if board[row][col] in seen:
+                        return False
+                    seen.add(board[row][col])
+        return True
+    
+    #Create test cases
+board1 = [["5","3",".",".","7",".",".",".","."],
+          ["6",".",".","1","9","5",".",".","."],
+          [".","9","8",".",".",".",".","6","."],    
+            ["8",".",".",".","6",".",".",".","3"],
+            ["4",".",".","8",".","3",".",".","1"],
+            ["7",".",".",".","2",".",".",".","6"],
+            [".","6",".",".",".",".","2","8","."],
+            [".",".",".","4","1","9",".",".","5"],
+            [".",".",".",".","8",".",".","7","9"]]
 
+board2 = [["8","3",".",".","7",".",".",".","."],
+          ["6",".",".","1","9","5",".",".","."],
+            [".","9","8",".",".",".",".","6","."],    
+                ["8",".",".",".","6",".",".",".","3"],
+                ["4",".",".","8",".","3",".",".","1"],
+                ["7",".",".",".","2",".",".",".","6"],
+                [".","6",".",".",".",".","2","8","."],
+                [".",".",".","4","1","9",".",".","5"],
+                [".",".",".",".","8",".",".","7","9"]]
+
+solution = Solution()
+print(solution.isValidSudoku(board1))  #Expected output: True
+print(solution.isValidSudoku(board2))  #Expected output: False
         
