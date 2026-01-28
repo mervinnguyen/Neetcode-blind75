14920cb2e48ac59392a789bcd89ae4fa8c4cf89e
Mervin Nguyen <mervin1@uci.edu>
2026-01-28 04:48:26 -0800

    Check each row and column

--- ValidSudoku.py
+++ ValidSudoku.py
@@ -4,3 +4,26 @@
 #3. Each of the nine 3x3 sub-boxes of the grid must contain the digits 1-9 without duplicates
 #Return true if the sudoku board ius valid, otherwise return false. 
 
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        #Check each row and make sure it contains 1-9 without duplicates
+        for row in range(9):
+            seen = set()
+            for i in range(9):
+                if board[row][i] == ".":
+                    continue
+                if board[row][i] in seen:
+                    return False        # return false if there are duplicates from 1-9
+            seen.add(board[row][i])
+        
+        #Check each column
+        for column in range(9):
+            seen = set()
+            for i in range(9):
+                if board[i][column] == ".":
+                    continue
+                if board[i][column] in seen:
+                    return False
+            seen.add(board[i][column])
+
+        
