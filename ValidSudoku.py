84e0c53ef518a6e4ca36bcec61a9b82206ae9c0b
Mervin Nguyen <mervin1@uci.edu>
2026-01-28 04:39:18 -0800

    Problem statement for Valid sudoku

---
+++ ValidSudoku.py
@@ -0,0 +1,6 @@
+#You are given a 9 x 9 sudoku board. A sudoku board is valid if the following rules are followed.
+#1. Each row ust contain the digits 1-9 without duplicates
+#2. Each column must contain the digits 1-9 without duplicates.
+#3. Each of the nine 3x3 sub-boxes of the grid must contain the digits 1-9 without duplicates
+#Return true if the sudoku board ius valid, otherwise return false. 
+
