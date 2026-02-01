40a817cd9904a584582835f68ab6d49f0dc86daa
Mervin Nguyen <mervin1@uci.edu>
2026-02-01 03:08:27 -0800

    ThreeSum solution

--- 3Sum.py
+++ 3Sum.py
@@ -2,3 +2,14 @@
 #nums[i] + nums[j] + nums[k] == 0, and the indices i,j, and k are all distinct
 #The output should not contain any duplicate triplets in any order.
 
+class Solution:
+    def threeSum(self, nums: list[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                for k in range(j+1, len(nums)):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        temp = [nums[i],nums[j],nums[k]]
+                        result.append(temp)
+        return result
