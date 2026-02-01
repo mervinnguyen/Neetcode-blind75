b936adcdaeed3e23af12a034ad46754bc12314f9
Mervin Nguyen <mervin1@uci.edu>
2026-02-01 03:03:01 -0800

    3Sum.py question

---
+++ 3Sum.py
@@ -0,0 +1,4 @@
+#Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] where 
+#nums[i] + nums[j] + nums[k] == 0, and the indices i,j, and k are all distinct
+#The output should not contain any duplicate triplets in any order.
+
