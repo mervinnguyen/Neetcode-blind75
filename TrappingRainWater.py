035bb2eb5aa38cd7acc727465cd1246bccfcd392
Mervin Nguyen <mervin1@uci.edu>
2026-02-06 01:14:13 -0800

    Solution to TrappingRainWater

--- TrappingRainWater.py
+++ TrappingRainWater.py
@@ -3,3 +3,21 @@
 
 #Return the maximum area of water that can be trapped between the bars.
 
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        #Two pointer approach
+        left = 0
+        right = len(height) - 1
+        #track leftMax and rightMax as the tallest walls seen
+        leftMax = height[left]
+        rightMax = height[right]
+        result = 0
+        while left < right:
+            if leftMax < rightMax:
+                left += 1
+                leftMax = max(leftMax, height[left])
+                result += leftMax - height[left]
+            else:
+                right -= 1
+                rightMax = max(rightMax, heigh[right])
+        return result
