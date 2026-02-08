1b5824438dfe3bfc043d6532e24b5c8bedc918a6
Mervin Nguyen <mervin1@uci.edu>
2026-02-08 03:10:50 -0800

    Solution for BestTimeToBuyAndSellstock

--- BestTimeToBuyAndSellStock.py
+++ BestTimeToBuyAndSellStock.py
@@ -6,6 +6,13 @@
 #Return the maximum profit you can achieve. You may choose to not make any transactions, in whuch case the profit would be 0.
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices: list[int]) -> int:
         #Use a nested for loop to iterate through all possible indices
         #keep track of maximum profit using maxProf
+        maxProf = 0
+        for i in range(len(prices)):
+            for j in range(i+1, len(prices)):
+                maxProf = max(maxProf, prices[j] - prices[i])
+        return maxProf
+
+print(Solution().maxProfit([1,5,3,6,4]))
