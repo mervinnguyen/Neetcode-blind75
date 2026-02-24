0d8c1f419f770331d98dcb254b23323cc238ef45
Mervin Nguyen <mervinnguyen123@gmail.com>
2026-02-24 02:33:35 -0800

    Solution

--- LongestRepeatingCharacterReplacement.py
+++ LongestRepeatingCharacterReplacement.py
@@ -11,3 +11,40 @@ class Solution:
     #Shrink it from the left unril it becomes valid again.
     #While sliding, keep track of the largest valid window again.
     #The largest window size is the answer.
+    def characterReplacement(self, s: str, k: int) -> int:
+        #dictionary to store each count of character in the current window
+        count = {}
+
+        left = 0 #left pointer
+        max_freq = 0    #highest frequency of a single character in the window
+        result = 0      #length of the longest valid window
+
+        #Expand the window by moving "right"
+        for right in range(len(s)):
+            char = s[right]
+
+            #Increase count of the current character
+            count[char] = count.get(char, 0) + 1
+
+            #Update the max frequency seen in the current window
+            max_freq = max(max_freq, count[char])
+
+            #Current window size
+            window_size = right - left + 1
+
+            #If the number of chars to change exceeds k, shrink the window from the left
+            #changes needed = window_size - max_freq
+            while window_size - max_freq > k:
+                left_char = s[left]
+                count[left_char] -= 1   #Character at "left" is leaving the window
+                left += 1
+                window_size = right - left + 1 #update window size 
+
+            #Update the result with the maximum valid window size found so far
+            result = max(result, window_size)
+        
+        #The result is teh length of the longest substring we can make repeating
+        return result
+    
+sol = Solution()
+print(sol.characterReplacement("XYYX", 2))
