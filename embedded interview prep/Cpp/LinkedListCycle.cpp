c3634fb790eb5fcc67d20a5ba1fe7a1a02b9fb0b
Mervin Nguyen <mervin1@uci.edu>
2026-03-28 01:28:30 -0700

    Refactor: Clean up comments and add Doxygen headers to LinkedListCycle.cpp

--- embedded interview prep/Cpp/LinkedListCycle.cpp
+++ embedded interview prep/Cpp/LinkedListCycle.cpp
@@ -1,23 +1,40 @@
-//Linked List Cycle
-//Problem:
-//Given the head of a linked list, determine if the linked list has a cycle in it.
 
-class Solution{
+/**
+ * @file LinkedListCycle.cpp
+ * @brief Detects if a singly linked list contains a cycle using Floyd's Tortoise and Hare algorithm.
+ *
+ * Uses two pointers moving at different speeds to determine if a cycle exists.
+ */
+
+/**
+ * @class Solution
+ * @brief Provides a method to check for cycles in a linked list.
+ */
+
+class Solution {
 public:
+    typedef struct Node{
+        int data;
+        struct Node *next;
+    } ListNode;
+    
+    /**
+     * @brief Determines if a linked list has a cycle.
+     *
+     * @param head Pointer to the head of the linked list.
+     * @return true if a cycle exists, false otherwise.
+     */
     bool hasCycle(ListNode *head) {
-        if (head == nullptr){
+        if (head == nullptr) {
             return false;
         }
-
         ListNode *slow = head;
         ListNode *fast = head;
-
         while (fast != nullptr && fast->next != nullptr) {
             slow = slow->next;
             fast = fast->next->next;
-
             if (slow == fast) {
-                return true; 
+                return true;
             }
         }
         return false;
