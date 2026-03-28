09ae556c2d8ca135cc65edfaf19a85b8101b4e16
Mervin Nguyen <mervin1@uci.edu>
2026-03-28 01:06:16 -0700

    Refactor MINmacro.c: add Doxygen header, clarify macro best practices, and remove redundant comments.

--- embedded interview prep/aCTest/MINmacro.c
+++ embedded interview prep/aCTest/MINmacro.c
@@ -1,4 +1,6 @@
-//Write the "standard" MIN macro. That is, a macro that takes two arguments and returns the smaller of the two arguments.
-
-#define MIN(A,B)    (((A) < (B)) ? (A) : (B))     //if A is less than B is true, then output A,else output B
-//small note: ake sure to paranthesize each argument
+/**
+ * @brief Standard MIN macro to return the smaller of two arguments.
+ *
+ * Always parenthesize macro arguments and the entire expression to avoid precedence bugs.
+ */
+#define MIN(A,B)    (((A) < (B)) ? (A) : (B))
