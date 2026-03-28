a937ed62617786881e689ea2cd6b22d35a3ebe90
Mervin Nguyen <mervin1@uci.edu>
2026-03-28 02:03:43 -0700

    docs: add Doxygen comments to embedded interview prep\aCTest\Interrupts.cpp

--- embedded interview prep/aCTest/Interrupts.cpp
+++ embedded interview prep/aCTest/Interrupts.cpp
@@ -1,14 +1,24 @@
 /**
- * @brief Example of incorrect interrupt service routine (ISR) declaration and best practices for ISRs in embedded systems.
- *
- * - ISRs cannot return a value.
- * - ISRs should not take parameters.
- * - Floating point operations are generally not safe or re-entrant in ISRs.
- * - ISRs should be short and avoid complex operations.
+ * @file Interrupts.cpp
+ * @brief Example of incorrect interrupt service routine (ISR) declaration and best practices for ISRs in embedded systems
+ * @author Author
+ * @date 2024-12-19
+ * @version 1.0.0
+ */
+
+/**
+ * @brief Incorrect example of interrupt service routine implementation
+ * @warning This function demonstrates multiple ISR violations:
+ *          - ISRs cannot return a value (void only)
+ *          - ISRs should not take parameters
+ *          - Floating point operations are not safe or re-entrant in ISRs
+ *          - ISRs should be short and avoid complex operations like printf
+ * @param[in] radius Radius value (INVALID - ISRs cannot have parameters)
+ * @return area Calculated area (INVALID - ISRs must return void)
+ * @note This code serves as an educational example of what NOT to do in an ISR
  */
 __interrupt double compute_area(double radius){
     double area = PI * radius * radius;
     printf("\nArea = %f", area);
     return area;
 }
-
