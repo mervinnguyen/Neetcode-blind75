d80096162c2e7b0887e46d21bbb9adf66caa23d7
Mervin Nguyen <mervin1@uci.edu>
2026-03-27 23:37:43 -0700

    Refactor MinStack.cpp : remove redundant comments, add Doxygen header for approach, and clarify code structure.

--- cpp solutions/MinStack.cpp
+++ cpp solutions/MinStack.cpp
@@ -1,50 +1,37 @@
-//Min Stack
-//Desgin a stack class that supports the push, pop, top, and getMin operations.
-//MinStack() initialized the stack object.
-//void push(int val) pushes the element on top of the stack
-//void pop() removes the element on top of the stack
-//int top() gets the element of the stack.
-//int getMin() retrieves the minimum element in the stack.
-
-//Input: ["MinStack", "push", 1, "push", 2, "push", 0, "getMin", "pop", "top", "getMin"]
-
-//Output: [null,null,null,null,0,null,2,1]
-
-// Explanation:
-// MinStack minStack = new MinStack();
-// minStack.push(1);
-// minStack.push(2);
-// minStack.push(0);
-// minStack.getMin(); // return 0
-// minStack.pop();
-// minStack.top();    // return 2
-// minStack.getMin(); // return 1
+/**
+ * @class MinStack
+ * @brief Stack supporting push, pop, top, and retrieving the minimum element in O(1) time.
+ *
+ * @details
+ * Approach:
+ * - Use two stacks: one for all values (main_stack), one for the minimum at each level (min_stack).
+ * - On push, add value to main_stack and the new minimum to min_stack.
+ * - On pop, remove from both stacks.
+ * - top() returns the top of main_stack.
+ * - getMin() returns the top of min_stack.
+ */
 
 #include <iostream>
 #include <stack>
-#include <algorithm>        //use math functions
+#include <algorithm>
 
 using namespace std;
-
-// 1) To push a value, append it to the stack
-// 2) To pop, remove the top element of the stack.
-// 3) To top, return the last element.
-// 4) To getmin, return the top of the min_stack.
 class MinStack {
 public:
-    stack<int> main_stack;  //stores all the values
-    stack<int> min_stack;   //keeps track of minimum value at each level.
+    stack<int> main_stack;
+    stack<int> min_stack;
 
-    MinStack() {
-    }
-
+    /*default constructor for MinStack class*/
+    MinStack() {}
+    
     void push(int val){
         main_stack.push(val);
         if (min_stack.empty()){
-            min_stack.push(val);        //handle the beginning case when min stack is empty
-        }
-        int minimum = min(val, min_stack.top());  //compute the new minimum between value pushed and current minimum on minStack.
-        min_stack.push(minimum);        //push new minimum onto min_stack;
+            min_stack.push(val);
+        } else {
+            int minimum = min(val, min_stack.top());  
+            min_stack.push(minimum);   
+        }   
     }
 
     void pop(){
@@ -62,15 +49,13 @@ public:
 };
 
 int main(){
-    MinStack stack;     //create object to class MinStack
-
+    MinStack stack; 
     stack.push(1);
     stack.push(2);
-    cout << "Current element top of the stack is: " << stack.top() << endl;
-    cout << "Current min stack is: " << stack.getMin() << endl;
+    cout << "Current element top of the stack is: " << stack.top() << endl;     //2
+    cout << "Current min stack is: " << stack.getMin() << endl;                 //1
     stack.pop();
-    cout << "Current element top of the stack is: " << stack.top() << endl;
-    cout << "Current min element of stack is: " << stack.getMin() << endl;
-    
+    cout << "Current element top of the stack is: " << stack.top() << endl;     //1
+    cout << "Current min element of stack is: " << stack.getMin() << endl;      //1
     return 0;
 }
