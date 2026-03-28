7378cf2b61a7425fe3189105540fb881dabe38c8
Mervin Nguyen <mervin1@uci.edu>
2026-03-28 01:05:26 -0700

    Refactor Interrupts.cpp: add Doxygen header, explain ISR best practices, and clarify why the example is incorrect.

--- embedded interview prep/aCTest/Interrupts.cpp
+++ embedded interview prep/aCTest/Interrupts.cpp
@@ -1,15 +1,14 @@
-//Interrupts are an important part of embedded systems. Consequently, many compiler vendors offer an extension to standard C to support interrupts. 
-//Typically, this new key word is __interrupt. tTeh following code uses __interrupt to define an ISR.
-//Comment on the code
-
+/**
+ * @brief Example of incorrect interrupt service routine (ISR) declaration and best practices for ISRs in embedded systems.
+ *
+ * - ISRs cannot return a value.
+ * - ISRs should not take parameters.
+ * - Floating point operations are generally not safe or re-entrant in ISRs.
+ * - ISRs should be short and avoid complex operations.
+ */
 __interrupt double compute_area(double radius){
     double area = PI * radius * radius;
-    printf("nArea = %f", area);
+    printf("\nArea = %f", area);
     return area;
 }
-
-// 1) ISRs cannot return a value
-// 2) Can't pass an ISR as a parameter. Can only be called inside a function.
-// 3) On compilers, floating point operations are not re-entrant. In other cases, one simply cannot do floating point in an ISR. 
-// 4) Interrupts should be short and sweet.
 
