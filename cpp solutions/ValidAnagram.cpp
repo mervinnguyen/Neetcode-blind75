120ae327c3b8caebaf9f9e62a4fb8ad03003b95a
Mervin Nguyen <mervin1@uci.edu>
2026-03-28 00:15:16 -0700

    Refactor ValidAnagram.cpp: remove obvious comments, add Doxygen header, and clarify code

--- cpp solutions/ValidAnagram.cpp
+++ cpp solutions/ValidAnagram.cpp
@@ -1,31 +1,31 @@
-//Valid anagram
-//Given two string s and t, return true if the two strings are anagrams of each other, otherwise return false.
-//An anagram is a string that contains the exact same characters as another string, but the order of the characters can be differnet.
-
-//ex 1.
-//input: s = "racecar", t = "carrace"
-//output: false
+/**
+ * @class Solution
+ * @brief Checks if two strings are anagrams by sorting and comparing.
+ *
+ * Sorts both strings alphabetically and compares them for equality.
+ * Time Complexity: O(n log n), Space Complexity: O(1) (ignoring sort implementation details)
+ */
 
 #include <iostream>
 #include <string>
 #include <algorithm>
+
 using namespace std;
 
-//1) Sort the string in alphabetical order 
 class Solution{
     public:
         bool isAnagram(string s, string t){
-            sort(s.begin(), s.end());    //sort string s in alphabetical order
-            sort(t.begin(), t.end());    //sort string t in alphabetical order
+            sort(s.begin(), s.end());
+            sort(t.begin(), t.end());
             return (s == t);
         }
 };
 
 int main(){
-    Solution sol;       //create object to class Solution
+    Solution sol;
     string s = "EmbeddedSystems";
     string t = "SystemsEmbedded";
     bool result = sol.isAnagram(s, t);
-
-    cout << (result ? "true" : "false") << endl;    //print out true or false depending on result 
+    cout << (result ? "true" : "false") << endl;
+    return 0;
 }
