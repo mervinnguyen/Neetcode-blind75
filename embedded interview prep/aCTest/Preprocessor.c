e6694d4012cdc5113a7eea95b830b581f7051de4
Mervin Nguyen <mervin1@uci.edu>
2026-03-28 01:07:15 -0700

    Suggested commit message:

--- embedded interview prep/aCTest/Preprocessor.c
+++ embedded interview prep/aCTest/Preprocessor.c
@@ -1,3 +1,6 @@
-//Using the #define statement, how would you declare a constant that returns the nuber of seconds in a year. Disregard leap years in your answer.
-
-#define SECONDS_IN_YEAR (365u * 24u * 60u * 60u)   //365 days in a year, 24 hours in a day, 60 minutes in an hour, 60 seconds in a minute
+/**
+ * @brief Macro to define the number of seconds in a non-leap year.
+ *
+ * 365 days/year × 24 hours/day × 60 minutes/hour × 60 seconds/minute.
+ */
+#define SECONDS_IN_YEAR (365u * 24u * 60u * 60u)
