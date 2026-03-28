295a641a21362ea03128bf0a99bd863dbf14c7e2
Mervin Nguyen <mervin1@uci.edu>
2026-03-28 02:03:10 -0700

    docs: add Doxygen comments to embedded interview prep\Cpp\ReferenceAliasUpdate.cpp

--- embedded interview prep/Cpp/ReferenceAliasUpdate.cpp
+++ embedded interview prep/Cpp/ReferenceAliasUpdate.cpp
@@ -1,33 +1,36 @@
-//Reference Alias Update
-//Given two signed 32-bit integers a and b, create a C++ reference taht aliases a.
-//Update the value of a byt adding b only through this reference.
-
-//Do not modify it directly after the reference is created.
-
-//Print the updated value of a.
-
-//Ex.
-//Input: 5 3
-//Output 8
+/**
+ * @file ReferenceAliasUpdate.cpp
+ * @brief Demonstrates C++ reference aliasing and indirect value modification
+ * @author Author
+ * @date 2024-12-19
+ * @version 1.0.0
+ */
 
 #include <iostream>
 #include <cstdint>
 using namespace std;
 
-
+/**
+ * @brief Main function that demonstrates reference aliasing for indirect value modification
+ * 
+ * Creates a reference alias to variable 'a' and uses it to update the value
+ * by adding variable 'b' without directly modifying 'a' after reference creation.
+ * 
+ * @return 0 on successful execution
+ * 
+ * @note Example: Input a=5, b=3 produces output 8
+ */
 int main(){
-    int32_t a, b;
+    int32_t a, b; /**< Input variables for the operation */
     cout << "Please enter a value for A: ";
     cin >> a;
     cout << "Please enter a value for B: ";
     cin >> b;
 
-    //Write code here
-    int &reference_a = a;        //set value to reference value a
-    int &reference_b = b;        //set value to reference value b 
+    int &reference_a = a; /**< Reference alias to variable a */
+    int &reference_b = b; /**< Reference alias to variable b */
 
-    //update reference value of a
-    reference_a += b;
+    reference_a += b; /**< Update a indirectly through its reference */
 
     cout << "The updated value of a is: " << a;
     return 0;
