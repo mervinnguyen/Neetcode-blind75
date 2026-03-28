9c6e05be02a704bd2d97e0428ebc5e8f554ddab5
Mervin Nguyen <mervin1@uci.edu>
2026-03-28 00:35:04 -0700

    Refactor DataDeclarations.c: remove obvious comments, add Doxygen headers, and clarify C variable and pointer declaration examples.

--- embedded interview prep/aCTest/DataDeclarations.c
+++ embedded interview prep/aCTest/DataDeclarations.c
@@ -1,26 +1,40 @@
-//Using the variable a, write down definitions for the following
-
-//a) an integer
+/**
+ * @brief Integer variable declaration.
+ */
 int a = 0;
 
-//b) pointer to an integer
+/**
+ * @brief Pointer to an integer.
+ */
 int *p;
 
-//c) pointer to a pointer to an integer
-int a **p;
+/**
+ * @brief Pointer to a pointer to an integer.
+ */
+int **pp;
 
-//d) an array of 10 integers
-int a[10];
+/**
+ * @brief Array of 10 integers.
+ */
+int arr[10];
 
-//e) an array of 10 pointers to integers
-int *a[10];
+/**
+ * @brief Array of 10 pointers to integers.
+ */
+int *parr[10];
 
-//f) A pointer to an array of 10 integers
-a[10] *p;       //correction: int (*a)(int);
+/**
+ * @brief Pointer to an array of 10 integers.
+ */
+int (*pArr)[10];
 
-//g) pointer to a function that takes an integer as an argument and returns and integer
-int function(int) *p;       //correction: int (*a)(int);
+/**
+ * @brief Pointer to a function that takes an integer as an argument and returns an integer.
+ */
+int (*funcPtr)(int);
 
-//h) an array of 10 pointers to functions that take an integer argument and return an integer.
-int function(int) *a[10];       //correction: int (*a[10])(int)
+/**
+ * @brief Array of 10 pointers to functions that take an integer argument and return an integer.
+ */
+int (*funcPtrArr[10])(int);
 
