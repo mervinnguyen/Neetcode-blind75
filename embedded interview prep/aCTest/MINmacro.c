3c99c94612571e35c9d7b041c62f0eb94cf61508
Mervin Nguyen <mervin1@uci.edu>
2026-03-28 02:03:47 -0700

    docs: add Doxygen comments to embedded interview prep\aCTest\MINmacro.c

--- embedded interview prep/aCTest/MINmacro.c
+++ embedded interview prep/aCTest/MINmacro.c
@@ -1,6 +1,21 @@
 /**
+ * @file MINmacro.c
+ * @brief Standard MIN macro implementation for returning the smaller of two values
+ * @author Author
+ * @date 2024-12-19
+ * @version 1.0.0
+ */
+
+/**
  * @brief Standard MIN macro to return the smaller of two arguments.
- *
+ * 
  * Always parenthesize macro arguments and the entire expression to avoid precedence bugs.
+ * 
+ * @param A First value to compare
+ * @param B Second value to compare
+ * @return The smaller of the two input values
+ * @note Arguments are evaluated twice - avoid using expressions with side effects
+ * @warning This macro evaluates each argument twice, which can cause issues with
+ *          function calls or increment/decrement operations
  */
 #define MIN(A,B)    (((A) < (B)) ? (A) : (B))
